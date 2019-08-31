16c66a12d5d35fb2b315e05538218956041d2f79
Maozu <32485372+Maozu@users.noreply.github.com>
2019-08-31 17:55:09 +0800

    del

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var week = data.getDay();
 var greet = new Vue({
     el: '#greet',
     data: {
-        message: 'Nice To Meet You, Maozu!'
+        message: 'Nice To Meet You'
     }
 });
 
@@ -14,41 +14,41 @@ var seed = Math.round(Math.random()*10);
 
 if(hour === 23||hour <= 4) {
     var morningword = new Array(4);
-    morningword[0] = 'Sleep Well, Maozu';
-    morningword[1] = 'Take Care Of Yourself';
-    morningword[2] = 'Sweetie!!';
-    morningword[3] = 'Watch out, Maozu';
+    morningword[0] = 'xxxxxxxx';
+    morningword[1] = 'xxxxxxxx';
+    morningword[2] = 'xxxxxxxx';
+    morningword[3] = 'xxxxxxxx';
     seed = Math.floor(Math.random()*4);
     greet.message = morningword[seed];
 }
 else if(hour <= 9) {
     var daybreak = new Array(5);
-    daybreak[0] = 'Good Morning, Maozu';
-    daybreak[1] = 'Morning, Sunshine~';
-    daybreak[2] = 'Good Day';
-    daybreak[3] = 'Take Care Of Yourself';
-    daybreak[4] = 'I Love You, Maozu';
+    daybreak[0] = 'xxxxxxxx';
+    daybreak[1] = 'xxxxxxxx';
+    daybreak[2] = 'xxxxxxxx';
+    daybreak[3] = 'xxxxxxxx';
+    daybreak[4] = 'xxxxxxxx';
     seed = Math.floor(Math.random()*5);
     greet.message = daybreak[seed];
 }
 else if(hour <= 12) {
-    greet.message = 'Rise And Shine, Sleepyhead';
+    greet.message = 'xxxxxxxx';
 }
 else if(hour <= 19) {
-    greet.message = 'Good Afternoon, Maozu';
+    greet.message = 'xxxxxxxx';
 }
 else {
     var night = new Array(3);
-    night[0] = 'Good Evening, Maozu';
-    night[1] = 'Nighty, night';
-    night[2] = 'すすすす、すき、だあいすき';
+    night[0] = 'xxxxxxxx';
+    night[1] = 'xxxxxxxx';
+    night[2] = 'xxxxxxxx';
     seed = Math.floor(Math.random()*3);
     greet.message = night[seed];
 }
 if(month === 12) {
     seed = Math.floor(Math.random()*2);
     if(seed) {
-        greet.message = 'Stay Warm, Darling';
+        greet.message = 'xxxxxxxx';
     }
 }
 
